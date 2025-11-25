dc38d2ab23ff02c7ef3e4dc19024bacf9527d09b
aptikasijunjung-crypto <aptikasijunjung@gmail.com>
2025-11-25 08:32:54 +0700

    deploy-github

--- app/Services/QrcodeServices.php
+++ app/Services/QrcodeServices.php
@@ -12,9 +12,9 @@ class QrcodeServices
         $namaqr = $nomor . '.png';
         $pathqr = Storage::path('qr_codes/' . $namaqr);
         $mime = Storage::mimeType('qr_codes/' . $namaqr);
-        // if (!file_exists($pathqr)) {
-        //     QrCode::format('png')->merge('/public/img/DtVw7MuU8AEPGlJ.jpg')->color(255, 0, 0)->size(100)->generate($nomor, Storage::path('qr_codes/' . $namaqr));
-        // }
+        if (!file_exists($pathqr)) {
+            QrCode::format('png')->merge('/public/img/DtVw7MuU8AEPGlJ.jpg')->color(255, 0, 0)->size(100)->generate($nomor, Storage::path('qr_codes/' . $namaqr));
+        }
 
         // $data = [
         //     'path' => $pathqr,
